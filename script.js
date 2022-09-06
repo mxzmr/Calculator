56862028f2a6805bb4acb2b1f6ad734cc4095169
mxzmr <maxzam88@gmail.com>
2022-09-06 16:03:10 +0300

    Refactored and add new functions
    reduced the size of the code add keyboard support and css style

--- script.js
+++ script.js
@@ -1,339 +1,179 @@
-function add() {
-    let sum = 0;
-    for (i = 0; i < arguments.length; i++){
-        sum += parseInt(arguments[i])
+let num1 = ''
+let num2 = ''
+let operator = ''
+let dot = ''
+let sum = ''
+const equalBtn = document.querySelector("[class='equal']").addEventListener("click", operate)
+const resetBtn = document.querySelector("[class='reset']").addEventListener("click", reset)
+const backBtn = document.querySelector("[class='backspace']").addEventListener("click", backSpace)
+const display = document.querySelector("[class='display']")
+const container = document.querySelector("[class='container']")
+document.querySelector("[class='dot']").addEventListener("click",dotLimit)
+
+function add(num1,num2) {
+    return Math.round((parseFloat(num1) + parseFloat(num2))*100)/100
+}
+function subtract(num1,num2){
+    return  Math.round((num1 - num2)*100)/100
+}
+function multiply(num1, num2){
+    return Math.round((num1 * num2) * 100)/ 100
+}
+function divide(num1, num2){
+    if(num2 == 0) return "Why divide by 0?"
+    else return Math.round((num1 / num2) * 100)/ 100
+}
+    
+
+function operate() {
+    if (operator =='+') {
+        if (num1 ==''){
+            sum = add(sum,num2)
+            display.textContent = sum
+        }
+        else{
+            sum = add(num1,num2)
+            display.textContent = sum;
+        }
     }
-    return sum
+    else if (operator =='-') {
+        if (num1 ==''){
+            sum = subtract(sum,num2)
+            display.textContent = sum
+        }
+        else{
+            sum = subtract(num1,num2)
+            display.textContent = sum
+        }
+    }
+    else if (operator =='*') {
+        if (num1 ==''){
+            sum = multiply(sum,num2)
+            display.textContent = sum
+        }
+        else{
+            sum = multiply(num1,num2)
+            display.textContent = sum
+        }
+    }
+    else if (operator =='/') {
+        if (num1 ==''){
+            sum = divide(sum,num2)
+            display.textContent = sum
+        }
+        else{
+            sum = divide(num1,num2)
+            display.textContent = sum
+        }
+    }
+    operator = ''
+    num1 = ''
+    num2 = ''
+    dot = ''
 }
 
-function subtract(){
-    let sum = arguments[0];
-        for (i = 1; i < arguments.length; i++){
-            sum -= arguments[i]
+function dotLimit() {
+    if (!operator){
+        if(!dot){
+        num1 += '.'
+        dot = '.'
+        display.textContent = num1
+        }
     }
-    return sum
+    else if (num2){
+        if(!dot){ 
+        num2 += '.'
+        dot = '.'
+        display.textContent = num2
+        }
+        else display.textContent = num2
+        }
 }
 
-function multiply(){
-    let sum = 1;
-    for (i = 0; i < arguments.length; i++){
-        sum *= arguments[i]
-    }
-    return sum
+function reset(){ 
+    display.textContent=''
+    num1 = ''
+    num2 = ''
+    operator = ''
+    sum = 0
+    dot = ''
 }
 
-function divide(){
-    let sum = arguments[0];
-    for (i = 1; i < arguments.length; i++){
-        
-        sum /= arguments[i]
+function backSpace(){ 
+    display.textContent=''
+    if (!operator){
+        num1 = num1.slice(0, -1)
+        display.textContent = num1
     }
-    return sum
+
+    else{
+        num2 = num2.slice(0, -1)
+        display.textContent = num2
+    }
 }
-
-
-function operate(){
-    let num1 = ''
-    let num2 = ''
-    let operator = ''
-    let sum = 0
-    const reset = document.querySelector("[class='reset']").addEventListener("click",()=> {
-        display.textContent=''
-        num1 = ''
-        num2 = ''
-        operator = ''
-        sum = 0
-    })
-    const display = document.querySelector("[class='display']")
-    document.querySelector("[class='7']").addEventListener("click",()=> {
+container.addEventListener("click",(event)=>{
+    if (event.target.id == "num" ){
         if (!operator){
-            num1 += 7
+            num1 += event.target.textContent
             display.textContent = num1
         }
 
         else{
-            num2 += 7
+            num2 += event.target.textContent
+            display.textContent = `${num1} ${operator} ${num2}`
+        }
+    }
+    else if (event.target.id == "operator" ){
+        
+        if (!operator){
+            operator = event.target.textContent
+            display.textContent = `${num1} ${operator}`
+            dot = ''
+        }
+        else{
+            if(!num2){
+                operator = event.target.textContent
+            }
+            else{
+                operate(num1,num2)
+                operator = event.target.textContent
+                display.textContent = `${operator}`
+            }
+        }
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if(event.key >= 0 && event.key <=9)
+        if (!operator){
+            num1 += event.key
+            display.textContent = num1
+        }
+        else{
+            num2 += event.key
             display.textContent = num2
         }
-        
-        })
-    
-    document.querySelector("[class='8']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 8
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 8
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='9']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 9
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 9
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='4']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 4
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 4
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='5']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 5
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 5
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='6']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 6
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 6
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='1']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 1
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 1
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='2']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 2
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 2
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='3']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 3
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 3
-                display.textContent = num2
-            }
-            
-        })
-    document.querySelector("[class='0']").addEventListener("click",()=> {
-        
-            if (!operator){
-                num1 += 0
-                display.textContent = num1
-            }
-    
-            else{
-                num2 += 0
-                display.textContent = num2
-            }
-            
-        })
-
-    document.querySelector("[class='+']").addEventListener("click",()=> {
+    else if (event.key == "+" || event.key == "-" || event.key == "*" 
+    || event.key == "/"){
         if (operator == ''){
-        operator = '+'
-        display.textContent = '+'
-        }
+            operator = event.key
+            }
         else {
-            if (num1 ==''){
-                sum = add(sum,num2)
-                display.textContent = sum
-                num1 = ''
-                num2 = ''
-            }
-            else{
-                sum = add(num1,num2)
-            display.textContent = sum
-            num1 = ''
-            num2 =''
-            }
+            operate(num1,num2)
+            operator = event.key
         }
-        })
-    document.querySelector("[class='-']").addEventListener("click",()=> {
-        if (operator == ''){
-            operator = '-'
-            display.textContent = '-'
-            }
-            else {
-                if (num1 ==''){
-                    sum = subtract(sum,num2)
-                    display.textContent = sum
-                    num1 = ''
-                    num2 = ''
-                }
-                else{
-                    sum = subtract(num1,num2)
-                display.textContent = sum
-                num1 = ''
-                num2 =''
-                }
-            }
-            })
-    document.querySelector("[class='*']").addEventListener("click",()=> {
-        if (operator == ''){
-            operator = '*'
-            display.textContent = '*'
-            }
-            else {
-                if (num1 ==''){
-                    sum = multiply(sum,num2)
-                    display.textContent = sum
-                    num1 = ''
-                    num2 = ''
-                }
-                else{
-                    sum = multiply(num1,num2)
-                display.textContent = sum
-                num1 = ''
-                num2 =''
-                }
-            }
-            })
-    document.querySelector("[class='/']").addEventListener("click",()=> {
-        if (operator == ''){
-            operator = '/'
-            display.textContent = '/'
-            }
-            else {
-                if (num1 ==''){
-                    sum = divide(sum,num2)
-                    display.textContent = sum
-                    num1 = ''
-                    num2 = ''
-                }
-                else{
-                    sum = divide(num1,num2)
-                display.textContent = sum
-                num1 = ''
-                num2 =''
-                }
-            }
-            })
+    }    
+    else if (event.key == "Backspace"){
+        backSpace()
+    }
+    else if (event.key == "Delete"){
+        reset()
+    }
+    else if (event.key == "Enter" || event.key == "=" ){
+        operate(num1, num2)
+    }
+    else if (event.key == "."){ 
+        dotLimit()
+    }
+})
 
-    document.querySelector("[class='=']").addEventListener("click",()=> {
-        // display.textContent = ''
-        if (operator =='+') {
-            if (num1 ==''){
-                sum = add(sum,num2)
-                display.textContent = sum
-                operator = ''
-                num1 = ''
-                num2 =''
-            }
-            else{
-                sum = add(num1,num2)
-            display.textContent = sum
-            operator = ''
-            num1 = ''
-            num2 =''
-            }
-            
-        }
-        
-        if (operator =='-') {
-            if (num1 ==''){
-                sum = subtract(sum,num2)
-                display.textContent = sum
-                operator = ''
-                num1 = ''
-                num2 =''
-            }
-            else{
-                sum = subtract(num1,num2)
-            display.textContent = sum
-            operator = ''
-            num1 = ''
-            num2 =''
-            }
-            
-        }
-        if (operator =='*') {
-            if (num1 ==''){
-                sum = multiply(sum,num2)
-                display.textContent = sum
-                operator = ''
-                num1 = ''
-                num2 =''
-            }
-            else{
-                sum = multiply(num1,num2)
-            display.textContent = sum
-            operator = ''
-            num1 = ''
-            num2 =''
-            }
-            
-        }
-
-        if (operator =='/') {
-            if (num1 ==''){
-                sum = divide(sum,num2)
-                display.textContent = sum
-                operator = ''
-                num1 = ''
-                num2 =''
-            }
-            else{
-                sum = divide(num1,num2)
-            display.textContent = sum
-            operator = ''
-            num1 = ''
-            num2 =''
-            }
-            
-        }
-        
-        
-        })
-        
-        
-}
-
-operate()
---
+++ testing.js
@@ -0,0 +1,10 @@
+let num1 = ''
+    let num2 = ''
+    let operator = ''
+    let dot = ''
+    let sum = 0
+   
+    const display = document.querySelector("[class='display']")
+    const displayFormula = document.getElementById('formula') 
+
+    displayFormula.textContent = 'wallak';
